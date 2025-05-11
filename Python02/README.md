d4a091ef8e5a27207f62db975c4cb92c590c99d8
superbunny38 <superbunny38@gmail.com>
2025-05-11 16:42:15 -0400

    local mcp server w python

--- Python02/README.md
+++ Python02/README.md
@@ -1,4 +1,19 @@
-# Build Remote MCP Servers w/ Python & FastMCP (Claude Integrations Tutorial)
-
+# Build Remote MCP Servers w/ Python & FastMCP
 ref:
-https://www.youtube.com/watch?v=bOYkbXP-GGo
+- https://www.youtube.com/watch?v=bOYkbXP-GGo
+- https://gofastmcp.com/getting-started/welcome
+
+
+# Building a local MCP Server
+Step 0. python server.py
+Step 1. npx @modelcontextprotocol/inspector
+Step 2. Open the inspector
+Step 3. Command: fastmcp
+Step 4. Arguments: run server.py:mcp
+Step 5. Connect
+Step 6. Tools
+Step 7. List Tools
+Step 8. Click on the necessary tool
+Step 9. Run Tool
+
+
--- Python02/server.py
+++ Python02/server.py
@@ -3,6 +3,30 @@ import datetime
 import pytz
 import os
 
+# client: ex.) Claude
+
 mcp = FastMCP(
-    name = 
+    name = "Current Date and Time",
+    instructions = "When you are asked for current date or time, call current_datetime() and pass along an optional timezone parameter (defaults to NYC)."
 )
+
+@mcp.tool()
+def current_datetime(timezone: str = "America/New_York") -> str:
+    """
+    Returns the current date and time as a string.
+    If you are asked for the current date or time, call this function.
+    Args:
+        timezone: Timezone name (e.g., 'UTC', 'US/Pacific', 'Europe/London').
+        Defaults to 'America/New_York'
+    """
+    
+    
+    try:
+        tz = pytz.timezone(timezone)
+        now = datetime.datetime.now(tz)
+        return now.strftime("%Y-%m0%d %H:%M:%S %Z")
+    except pytz.exceptions.UnknownTimeZoneError:
+        return f"Error: Unknown timezone '{timezone}'. Please use a valid timezone name."
+    
+if __name__ == "__main__":
+    mcp.run()
