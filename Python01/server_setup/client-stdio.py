4c89092da12c266caaabd2e1f54a5d31a2bb5528
superbunny38 <superbunny38@gmail.com>
2025-05-10 20:34:55 -0400

    client-stdio.py init

--- Python01/server_setup/client-stdio.py
+++ Python01/server_setup/client-stdio.py
@@ -2,3 +2,11 @@ import asyncio
 import nest_asyncio
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
+
+nest_asyncio.apply()
+
+async def main():
+    server_params = StdioServerParameters(
+        command = "python",
+        
+    )
