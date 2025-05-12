242b77c5f0de12508c7f1070f76440808b6e7c8a
superbunny38 <superbunny38@gmail.com>
2025-05-12 01:11:42 -0400

    custom tool updated

--- Python04/tools.py
+++ Python04/tools.py
@@ -16,3 +16,19 @@ search_tool = Tool(
 
 api_wrapper = WikipediaAPIWrapper(top_k_results=3, doc_content_chars_max=100)
 wiki_tool = WikipediaQueryRun(api_wrapper=api_wrapper)
+
+
+# Custom Tool
+def save_to_txt(data: str, filename: str = "research_output.txt"):
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    formatted_text = f"--- Research Output ---\nTimestamp: {timestamp}\n\n{data}\n\n"
+    
+    with open(filename, "a", encoding = "utf-8") as f:
+        f.write(formatted_text)
+    
+    return f"Data successfully saved to {filename}"
+
+save_tool = Tool(name = "save_text_to_file",
+                 func = save_to_txt,
+                 description = "Saves structured research data to a text file.",
+                 )
