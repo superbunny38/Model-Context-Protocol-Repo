ef313d8ee73b638f6284385b1f6dd6cc1b23ac8c
superbunny38 <superbunny38@gmail.com>
2025-05-15 14:27:45 -0400

    ref client code done

---
+++ Python10/reference_client_code.py
@@ -0,0 +1,37 @@
+from mcp import ClientSession, StdioServerParameters, types
+from mcp.client.stdio import stdio_client
+import asyncio
+
+
+# Create server parameters for stdio connection
+server_params = StdioServerParameters(
+    command = "uv", #Executable
+    args = "run example_servre.py", # Command line arguments
+    env = None, # Environment variables
+)
+
+async def run():
+    # Launch the server as a subprocess & returns the read and write streams
+    # read: the stream that the client will use to read messages from the server
+    # write: the stream that client will use to write messages to the server
+    
+    async with stdio_client(server_params) as (read, write):
+        # the Client session is used to initiate the connection
+        # and send requests to server
+        
+        async with ClientSession(read, write) as session:
+            # Initialie the connection (1:1 connection with the server)
+            await session.initialize()
+            
+            # List available tools
+            tools = await session.list_tools()
+            
+            #will call the chat loop here
+            # ....
+            
+            # Call a tool: this will be in the process_query method
+            
+            result = await session.call_tool("tool-name", arguments={"arg1": "value"})
+            
+        if __name__ == "__main__":
+            asyncio.run(run())
